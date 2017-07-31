efe57cc8604d8ba96a8b4ec1a1bd2bff28229d70
nmc <987128747@qq.com>
2017-07-31 12:54:09 +0800

    提交

---
+++ 项目管理工具/项目管理工具/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  项目管理工具
+//
+//  Created by NMC on 2017/7/31.
+//  Copyright © 2017年 NMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
